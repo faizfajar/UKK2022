c9bb27dde92fd2498580ed005347749fd12e1764
faizfajar <faizaprianda112@gmail.com>
2022-04-08 13:08:50 +0700

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,7 +70,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         // dd($data);
-        $file = $data;
+        // $file = $data;
     //    $con Storage::disk('my_disk');
         // $file = fopen('/storage/app/user-created.txt',"User Yang berhasil Daftar");
         // fwrite($file, `'$data'`);
