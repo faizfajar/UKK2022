79c3123ab9a19597196a4e6b0e2a43df9d20c74a
faizfajar <faizaprianda112@gmail.com>
2022-04-08 12:56:19 +0700

    update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'faizfajar',
+            'email' => 'faiz@gmail.com',
+            'nik' => '1920100289',
+            'password' => bcrypt('password'),
+        ]);
+    }
+
+}
